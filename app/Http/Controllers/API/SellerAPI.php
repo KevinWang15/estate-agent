c94e462138701615ec08f9f8587f367b7a7ce5bb
Kevin Wang <453587854@qq.com>
2016-04-13 09:02:07 +0800

    允许用户编辑已经发布的房产

--- app/Http/Controllers/API/SellerAPI.php
+++ app/Http/Controllers/API/SellerAPI.php
@@ -43,19 +43,31 @@ trait SellerAPI
         $user = APIUtilProvider::getUser();
         $estate = Estate::find(intval(Input::get('id')));
         if (!$estate)
-            APIResponseBuilder::err(-5, "");
+            APIResponseBuilder::err(-5, "房产不存在");
+        if ($estate->user_id != $user->id)
+            APIResponseBuilder::err(-6, "不是你的房产");
         $estate->is_hidden = true;
         $estate->save();
         APIResponseBuilder::respond();
 
     }
 
-    public function postNewEstate()
+    public function postEditEstate()
     {
+
         $user = APIUtilProvider::getUser();
         if ($user->user_type != 1)
             APIResponseBuilder::err(-3, '');
-        $estate = new Estate();
+        $id = intval(@Input::get("id", 0));
+        if ($id == 0) {
+            $estate = new Estate();
+        } else {
+            $estate = Estate::find($id);
+            if (!$estate)
+                APIResponseBuilder::err(-5, "房产不存在");
+            if ($estate->user_id != $user->id)
+                APIResponseBuilder::err(-6, "不是你的房产");
+        }
         $estate->user_id = $user->id;
         $estate->city = @Input::get("city", "");
         $estate->district = @Input::get("district", "");
@@ -69,15 +81,17 @@ trait SellerAPI
         $estate->is_hidden = 0;
         $estate->save();
 
-        //随机为新添加的房产设置中介
-        $agents_array = $agents = \App\User::where('user_type', 2)->get()->toArray();
+        if ($id == 0) {
+            //随机为新添加的房产设置中介
+            $agents_array = $agents = \App\User::where('user_type', 2)->get()->toArray();
 
-        shuffle($agents_array);
-        $builder = [];
-        for ($i = 0; $i < 5; $i++) {
-            $builder[] = "({$agents_array[$i]["id"]},$estate->id)";
+            shuffle($agents_array);
+            $builder = [];
+            for ($i = 0; $i < 5; $i++) {
+                $builder[] = "({$agents_array[$i]["id"]},$estate->id)";
+            }
+            \DB::insert("insert into agent_estate (agent_id,estate_id) values " . implode(',', $builder));
         }
-        \DB::insert("insert into agent_estate (agent_id,estate_id) values " . implode(',', $builder));
 
         APIResponseBuilder::respond();
     }
