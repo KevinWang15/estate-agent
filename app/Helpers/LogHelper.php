76766585294c5bc0de39a1ca8e300c0cbdbfaa63
Kevin Wang <453587854@qq.com>
2016-04-22 00:12:12 +0800

    fix LogHelper bug

--- app/Helpers/LogHelper.php
+++ app/Helpers/LogHelper.php
@@ -12,16 +12,17 @@ class LogHelper
     {
         if (empty(@self::$loggers[$logName])) {
             self::$loggers[$logName] = new Logger($logName);
+            /** @var Logger $logInstance */
+            $logPath = storage_path('/logs/' . $logName . '.log');
+
+            if (!file_exists($logPath))
+                touch($logPath);
+
+            self::$loggers[$logName]->pushHandler(new StreamHandler($logPath, Logger::DEBUG));
         }
 
-        /** @var Logger $logInstance */
         $logInstance = self::$loggers[$logName];
-        $logPath = storage_path('/logs/' . $logName . '.log');
 
-        if (!file_exists($logPath))
-            touch($logPath);
-
-        $logInstance->pushHandler(new StreamHandler($logPath, Logger::DEBUG));
         $logInstance->addDebug($message, $context);
     }
 
